c3ca11ece7668305e194774783accb5b78368368
LutanTech <lutancorpinfoteam@gmail.com>
2026-07-15 00:05:57 +0300

    Update app.py: replace app.run() with a print statement for debugging and remove newline at the end of the file.

--- app.py
+++ app.py
@@ -851,4 +851,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    print('iiiiiiiiiiiii')
+    # app.run(host="0.0.0.0", port=5000, debug=True)
